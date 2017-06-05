37800c1528ac5779c51615c3b37c50ecea80d7ca
william <williamsousadasilva@gmail.com>
2017-06-05 17:49:13 -0300

    Finalizado - Implementando a exclusão

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -1,3 +1,22 @@
 /**
  * Created by william on 05/06/2017.
  */
+
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function (event) {
+
+    var button = $(event.relatedTarget); // "pega" o botão que chamou o modal
+
+    var codigoTitulo = button.data('codigo');
+    var descricaoTitulo = button.data('descricao');
+
+    var modal = $(this);
+    var form = modal.find('form'); // seleciona o formulário do modal
+    var action = form.attr('action'); // seleciona o atributo action do formulário
+    if (!action.endsWith('/')) {
+        action += '/'; // adiciona '/' se não tiver
+    }
+
+    form.attr('action', action + codigoTitulo);
+
+    modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>'+descricaoTitulo+'</strong>');
+});
