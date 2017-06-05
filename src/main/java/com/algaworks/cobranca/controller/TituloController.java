94d04d7d326d9155dbf656db42e7ddd0e209b1b6
williamsilvaec <williamsousadasilva@gmail.com>
2017-06-04 23:03:49 -0300

    Validações com Bean Validation

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -5,6 +5,8 @@ import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,16 +29,21 @@ public class TituloController {
     public ModelAndView novo() {
 
         ModelAndView mv = new ModelAndView("CadastroTitulo");
-
+        mv.addObject(new Titulo());
         return mv;
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public ModelAndView salvar(Titulo titulo) {
+    public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+
+        if (errors.hasErrors()) {
+            return mv;
+        }
 
         titulos.save(titulo);
 
-        ModelAndView mv = new ModelAndView("CadastroTitulo");
         mv.addObject("mensagem", "Título salvo com sucesso!");
         return mv;
     }
--- src/main/java/com/algaworks/cobranca/model/Titulo.java
+++ src/main/java/com/algaworks/cobranca/model/Titulo.java
@@ -1,10 +1,15 @@
 package com.algaworks.cobranca.model;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -19,12 +24,18 @@ public class Titulo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long codigo;
 
+    @NotEmpty(message = "Descrição é obrigatória")
+    @Size(max = 60, message = "A descrição não pode conter mais de 60 caracteres")
     private String descricao;
 
+    @NotNull(message = "Data de vencimento é obrigatória")
     @DateTimeFormat(pattern = "dd/MM/yyyy")
     @Temporal(TemporalType.DATE)
     private Date dataVencimento;
 
+    @NotNull(message = "Valor é obrigatório")
+    @DecimalMin(value = "0.01", message = "Valor não pode ser menor que 0,01")
+    @DecimalMax(value= "9999999.99", message = "Valor não pode ser maior que 9.999.999,99")
     @NumberFormat(pattern = "#,##0.00")
     private BigDecimal valor;
 
