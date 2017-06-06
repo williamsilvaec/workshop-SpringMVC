ed50b4f31b71d7d4b7519e3fa7e9d9024cdb6e60
william <williamsousadasilva@gmail.com>
2017-06-06 09:28:05 -0300

    Máscara de entrada de valores

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -24,5 +24,6 @@ $('#confirmacaoExclusaoModal').on('show.bs.modal', function (event) {
 $(function(){
 
     $('[rel="tooltip"]').tooltip();
+    $('.js-currency').maskMoney({decimal: ',', thousands: '.', allowZero: true});
 
 });
