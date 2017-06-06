603715078139eac5dd8ad550746448aa74ec59ed
williamsilvaec <williamsousadasilva@gmail.com>
2017-06-06 00:02:02 -0300

    Plugin para calendário. Adicionado validação para o campo dataVencimento no servidor.

--- src/main/java/com/algaworks/cobranca/controller/TituloController.java
+++ src/main/java/com/algaworks/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import com.algaworks.cobranca.model.StatusTitulo;
 import com.algaworks.cobranca.model.Titulo;
 import com.algaworks.cobranca.repository.Titulos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
@@ -42,9 +43,14 @@ public class TituloController {
             return "CadastroTitulo";
         }
 
-        titulos.save(titulo);
-        attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
-        return "redirect:/titulos/novo";
+        try {
+            titulos.save(titulo);
+            attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+            return "redirect:/titulos/novo";
+        } catch (DataIntegrityViolationException e) {
+            errors.rejectValue("dataVencimento",null, "Formato de data inválido!");
+            return "CadastroTitulo";
+        }
     }
 
     @RequestMapping
